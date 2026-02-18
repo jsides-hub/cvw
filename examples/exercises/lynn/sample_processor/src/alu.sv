553a227f333d6a716e2d442d3b18f29421abfdb8
jsides-hub <jsides@g.hmc.edu>
2026-02-18 15:57:14 -0800

    ALU updates

--- examples/exercises/lynn/sample_processor/src/alu.sv
+++ examples/exercises/lynn/sample_processor/src/alu.sv
@@ -9,6 +9,8 @@ module alu(
         output  logic [31:0]    ALUResult, IEUAdr
     );
 
+    // Sub on when slt, sltu, sub, sra, srai
+
     logic [31:0] CondInvb, Sum, SLT, SLTU, Right;
     logic ALUOp, Sub, Overflow, Neg, LT;
     logic [2:0] ALUFunct;
@@ -24,10 +26,11 @@ module alu(
     assign Overflow = (SrcA[31] ^ SrcB[31]) & (SrcA[31] ^ Sum[31]);
     assign Neg = Sum[31];
     assign LT = Neg ^ Overflow;
+    assign LTU = Overflow;
     assign SLT = {31'b0, LT};
-    assign SLTU = SLT; // TODO : implement SLTU
+    assign SLTU = {31'b0, LTU};
+    assign Right = Sub ? (SrcA >>> SrcB) : (SrcA >> SrcB);
     assign ALUFunct = Funct3 & {3{ALUOp}}; // Force ALUFunct to 0 to Add when ALUOp = 0
-    assign Right = Sub ? SrcA >>> SrcB : SrcA >> SrcB;
 
     always_comb begin
         case (ALUFunct)
@@ -36,7 +39,7 @@ module alu(
             3'b010: ALUResult = SLT; // slt
             3'b011: ALUResult = SLTU; // sltu
             3'b100: ALUResult = SrcA ^ SrcB; // xor
-            3'b101: ALUResult = SrcA; // srl or sra
+            3'b101: ALUResult = Right; // srl or sra
             3'b110: ALUResult = SrcA | SrcB; // or
             3'b111: ALUResult = SrcA & SrcB; // and
             default: ALUResult = 'x;
