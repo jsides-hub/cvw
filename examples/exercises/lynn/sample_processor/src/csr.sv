63051c439fc31191ebbbcdad203ed191de4cb36a
jsides-hub <jsides@g.hmc.edu>
2026-03-07 22:25:09 -0800

    Counters implemented, zictr tested

---
+++ examples/exercises/lynn/sample_processor/src/csr.sv
@@ -0,0 +1,65 @@
+// riscvsingle.sv
+// RISC-V single-cycle processor
+// David_Harris@hmc.edu 2020
+
+module csr(
+        input   logic           clk,
+        input   logic           reset,
+        input   logic[31:0]     Instr,
+        input   logic           BranchTaken,
+        input   logic           MemEn,
+        input   logic           WriteEn,
+        output  logic[31:0]     CSRResult
+    );
+
+    genvar i;
+    logic CSROp;
+    logic[2:0] Funct3;
+    assign CSROp = (Instr[6:0] == 7'b1110011);
+    assign Funct3 = Instr[14:12];
+
+    // Updating counters
+    logic [31:0] Events;
+    logic [63:0] CSRVals[10:0];
+
+    for(i = 0; $unsigned(i) < 10; i=i+1) begin
+        always_ff @(posedge clk)
+            if (reset)  CSRVals[i] = '0;
+            else        CSRVals[i] = CSRVals[i] + {{63{1'b0}}, Events[i]};
+    end
+
+    assign Events[0] = 1'b1; // Number of cycles
+    assign Events[1] = Events[0]; // Time (= cycles)
+    assign Events[2] = Events[0]; // Instructions retired (one cycle per)
+    assign Events[3] = ({Instr[14:12], Instr[6:0]} == 10'b0000110011); // Add instructions
+    assign Events[4] = (Instr[6:0] == 7'b1100011); // Branches evaluated
+    assign Events[5] = BranchTaken; // Branches taken
+    assign Events[6] = ({Instr[6:4], Instr[2:0]} == 6'b110111); // Jumps taken
+    assign Events[7] = WriteEn; // Memory writes
+    assign Events[8] = MemEn; // Memory ops
+    assign Events[9] = CSROp; // CSR ops
+    assign Events[10] = (({Instr[6], Instr[4:0]} == 6'b010011) & (Funct3 != 3'b000)); // Non-add ALU ops
+
+
+
+    // assign Events[5] = ;
+
+    // Read value
+    logic [11:0] CSRVal;
+    assign CSRVal = Instr[31:20];
+
+    always_comb
+        case(CSRVal)
+            12'hC00: CSRResult = CSRVals[0][31:0];
+            12'hC01: CSRResult = CSRVals[1][31:0];
+            12'hC02: CSRResult = CSRVals[2][31:0];
+            12'hC03: CSRResult = CSRVals[3][31:0];
+            12'hC80: CSRResult = CSRVals[0][63:32];
+            12'hC81: CSRResult = CSRVals[1][63:32];
+            12'hC82: CSRResult = CSRVals[2][63:32];
+            12'hC83: CSRResult = CSRVals[3][63:32];
+            default: CSRResult = '0;
+        endcase
+
+
+endmodule
