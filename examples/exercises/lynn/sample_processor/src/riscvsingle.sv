2b7c0ed44ce984336b2d94c186de98129e8736c0
jsides-hub <jsides@g.hmc.edu>
2026-04-29 00:25:24 -0700

    Mul implemented, untested

--- examples/exercises/lynn/sample_processor/src/riscvsingle.sv
+++ examples/exercises/lynn/sample_processor/src/riscvsingle.sv
@@ -21,9 +21,9 @@ module riscvsingle (
         output  logic [2:0]     Funct3M
     );
 
-    logic [31:0]    InstrD, PCD, PCE, ReadDataW, CSRResultW, FSrcBE, ResultW;
+    logic [31:0]    InstrD, PCD, PCE, ReadDataW, CSRResultW, FSrcAE, FSrcBE, ResultW, MulResW;
     logic [31:0]    IEUResultW, IEUResultM, IEUResultE, IEUAdrE, ImmExtE;
-    logic           RegWriteE, RegWriteM, RegWriteW, MemEnE, MemEnD, PCSrcE;
+    logic           RegWriteE, RegWriteM, RegWriteW, MemEnE, MemEnD, PCSrcE, MulE, MulM;
     logic [1:0]     ResultSrcW, ResultSrcE, ALUControlE;
     logic [2:0]     Funct3E;
     logic [4:0]     RdE, RdM, RdW;
@@ -39,21 +39,23 @@ module riscvsingle (
     ieu ieu(.clk, .reset, .InstrD, .PCD,
             .ForwardAE, .ForwardBE, .StallE, .FlushE,
             .RegWriteW, .ResultSrcW, .IEUResultW, .ReadDataW, .CSRResultW, .RdW, .IEUResultM,
-            .RegWriteE, .ResultSrcE, .MemEnE, .IEUResultE, .IEUAdrE, .ImmExtE, .FSrcBE, .Funct3E, .RdE,
+            .RegWriteE, .ResultSrcE, .MulE, .MemEnE, .IEUResultE, .IEUAdrE, .ImmExtE, .FSrcAE, .FSrcBE, .Funct3E, .RdE,
             .PCE, .PCSrcE, .WriteByteEnE, .ResultW,
             .Rd1D, .Rd2D, .Rd1E, .Rd2E, .MemEnD, .JumpE, .BranchE, .LoadE, .CSROpE, .ALUControlE);
-    lsu lsu(.clk, .reset, .PCE, .RegWriteE, .ResultSrcE, .MemEnE, .WriteByteEnE, .IEUResultE, .IEUResultM, .IEUAdrE, .WriteDataE(FSrcBE), .Funct3E, .RdE, .LoadE, .CSROpE,
+    lsu lsu(.clk, .reset, .PCE, .RegWriteE, .ResultSrcE, .MulE, .MemEnE, .WriteByteEnE, .IEUResultE, .IEUResultM, .IEUAdrE, .WriteDataE(FSrcBE), .Funct3E, .RdE, .LoadE, .CSROpE,
             .StallM, .FlushM, .StallW, .FlushW, .ReadDataM,
-            .IEUAdrM, .WriteDataM, .MemEnM, .WriteByteEnM, .Funct3M,
+            .IEUAdrM, .WriteDataM, .MulM, .MemEnM, .WriteByteEnM, .Funct3M,
             .RegWriteW, .ResultSrcW, .IEUResultW, .ReadDataW, .RdW, .RdM, .LoadM, .CSROpM, .RegWriteM);
 
     hazard hazard(.PCSrcE,
-    .Rd1D, .Rd2D, .Rd1E, .Rd2E, .RdE, .RdM, .RdW, .MemEnD, .MemEnE, .LoadE, .LoadM, .CSROpE, .CSROpM, .RegWriteM, .RegWriteW,
+    .Rd1D, .Rd2D, .Rd1E, .Rd2E, .RdE, .RdM, .RdW, .MemEnD, .MemEnE, .LoadE, .LoadM, .MulM, .CSROpE, .CSROpM, .RegWriteM, .RegWriteW,
     .StallF, .StallD, .FlushD, .StallE, .FlushE, .StallM, .FlushM, .StallW, .FlushW, .ForwardAE, .ForwardBE);
 
-//TODO: Correct signals for InstRetW and BranchEvalE
+        //TODO: Correct signals for InstRetW and BranchEvalE
     csr csr(.clk, .reset, .StallM, .FlushM, .StallW, .FlushW, .CSRVal(ImmExtE[11:0]), .Funct3E, .ResultSrcE, .ALUControlE,
     .InstRetW(ResultW != 32'b0), .BranchEvalE(BranchE), .BranchTakenE(BranchE&PCSrcE), .JumpE, .WriteEnM, .MemEnM, .CSRResultW);
+
+    multiplier mul(.clk, .reset, .StallM, .FlushM, .StallW, .FlushW, .FSrcAE, .FSrcBE, .Funct3E, .MulResW);
 
     assign WriteEnM = |WriteByteEnM;
 endmodule
